2e3bffb95e636f2d6fdc91bad10e6076739e6b17
250072-dot <250072@newuu.uz>
2025-10-22 12:54:07 +0500

    changed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
 using namespace std;
+
+    // problem 1
+    void problem1(int n=10) {
+      cout<<n<<endl;
+        if (n == 0) return;
+        problem1(n-1);
+    }
+
+    //problem 2
+    void problem2(int n=50) {
+      cout << n <<endl;
+        if (n == 1) return;
+        problem2(n-1);
+    }
+    //problem 3
+   int problem3(int n,int sum = 0) {
+      if (n==0) cout<<sum;
+      return sum + problem3(n-1,sum);
+    }
+
+
+  //problem 4
+    int problem4(int n) {
+        if (n == 1) return 1;
+        return problem4(n-1)*n;
+    }
+
+  //problem 5
+
+
+
+
+
+
+
 int main() {
-cout << "Hello World!" << endl;}
+        cout<<problem4(5);
+      []
+    }
